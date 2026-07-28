59e950b2a06344f5ed3077462e0b921a5245119c
MiltonLelis <135756305+MiltonLelis@users.noreply.github.com>
2026-07-28 01:07:21 +0000

    Desafio: Nível Aventureiro

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,11 +1,29 @@
+// Classe Guerreiro representa um tipo específico de Personagem no jogo.
+// Ela herda (estende) as características e comportamentos da classe Personagem.
 public class Guerreiro extends Personagem {
+    // Atributo específico do Guerreiro para calcular ou armazenar sua força física
     double forca;
 
+    // Construtor da classe Guerreiro.
     public Guerreiro(String nome, int nivel, int pontosDeVida, double poderBase, double forca) {
+        // Chama o construtor da classe pai (Personagem) repassando os parâmetros
+        // O valor "Guerreiro" é passado fixo para definir a classe/categoria
         super(nome, "Guerreiro", nivel, pontosDeVida, poderBase);
+        
+        // Inicializa o atributo específico desta classe
         this.forca = forca;
     }
 
+    // Sobrescreve (@Override) o método da classe pai (Personagem)
+    // para exibir o Status do Guerreiro.
+    @Override
+    public void exibirStatus() {
+        super.exibirStatus();
+        System.out.println("Força: " + forca);
+    }
+
+    // Sobrescreve (@Override) o método da classe pai (Personagem)
+    // para definir a habilidade especial exclusiva do Guerreiro.
     @Override
     public void usarHabilidadeEspecial() {
         System.out.println("Guerreiro " + nome + " ataca com Espada Flamejante!");
--- Mago.java
+++ Mago.java
@@ -1,11 +1,29 @@
+// Classe Mago representa uma especialização de Personagem focada em magia.
+// Ela herda (estende) as características e comportamentos da classe Personagem.
 public class Mago extends Personagem {
+    // Atributo específico do Mago para armazenar seu nível de inteligência/poder mágico
     double inteligencia;
 
+    // Construtor da classe Mago.
     public Mago(String nome, int nivel, int pontosDeVida, double poderBase, double inteligencia) {
+        // Chama o construtor da classe pai (Personagem), enviando os dados gerais
+        // e definindo o tipo/classe fixo como "Mago"
         super(nome, "Mago", nivel, pontosDeVida, poderBase);
+        
+        // Inicializa o atributo específico de inteligência desta classe
         this.inteligencia = inteligencia;
     }
 
+    // Sobrescreve (@Override) o método da classe pai (Personagem)
+    // para exibir o Status do Guerreiro.
+    @Override
+    public void exibirStatus() {
+        super.exibirStatus();
+        System.out.println("Inteligência: " + inteligencia);
+    }
+
+    // Sobrescreve (@Override) o método da classe pai (Personagem)
+    // para definir a habilidade especial exatada do Mago.
     @Override
     public void usarHabilidadeEspecial() {
         System.out.println("Mago " + nome + " lança Bola de Fogo!");
